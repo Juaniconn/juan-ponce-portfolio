d355f58ba29545311b370cbdad51f3716e89bfad
Juan Ponce <me.juanponce@gmail.com>
2020-05-27 11:45:25 -0600

    Favicon

--- src/components/molecules/experience&Education.js
+++ src/components/molecules/experience&Education.js
@@ -61,7 +61,7 @@ const ExpAndEdu = () => {
                 <Typography style={{color: '#fff', fontWeight: 'bolder', fontSize: '1.2rem', marginBottom: '2rem'}} variant="h6">Experience</Typography>
                 <Box className={classes.expAndEduWrapper}>
                     <Box className={classes.expAndEduItem}>
-                        <Typography style={{color: '#56D8B7', fontWeight: 'bold', fontSize: '1rem'}} variant="subtitle1">2019 - Present</Typography>
+                        <Typography style={{color: '#56D8B7', fontWeight: 'bold', fontSize: '1rem'}} variant="subtitle1">2019 - 2020</Typography>
                         <Typography style={{color: '#fff', fontSize: '1.2rem', fontWeight: 'bold'}} variant="h6">Frontend Developer - Primal Technologies</Typography>
                         <Typography style={{color: '#fff'}} variant="body1">Monitored in technical aspects of the front-end from the beginning of process to delivery terms.</Typography>
                     </Box>
